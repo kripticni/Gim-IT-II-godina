6e6f7af831640c870180afb88191d1552093f140
kripticni <157402936+kripticni@users.noreply.github.com>
2024-05-10 12:02:41 +0200

    Gradivo sa casa

--- Stack/izvorniKod.c
+++ Stack/izvorniKod.c
@@ -1,44 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct element{
-	int podatak;
-	struct element *sledeci;
+typedef struct Element{
+    int podatak;
+    struct Element *sledeci;
 }Element;
 
-void push(Element **vrh, int x){
-	Element *novi=(Element*)malloc(sizeof(Element));
-	novi->podatak=x;
-	novi->sledeci=(*vrh);
-	(*vrh)=novi;
+void stampaj(Element *vrh){
+    while(vrh!=NULL){
+        printf("%i", vrh->podatak);
+        vrh=vrh->sledeci;
+    }
 }
 
-void pop(Element **vrh, int *x){
-	Element *novi=(*vrh);
-	(*vrh)=(*vrh)->sledeci;
-	*x=(novi)->podatak;
-	free(novi);
+Element* push(Element* vrh,int x){
+    Element *novi;
+    novi=(Element*)malloc(sizeof(Element));
+    novi->podatak=x;
+    novi->sledeci=vrh;
+    vrh=novi;
+    return vrh;
 }
 
-void stampaj(Element* vrh){
-	while(vrh!=NULL){
-		printf("%i", vrh->podatak);
-		vrh=vrh->sledeci;
-	}
+
+void pop(Element **vrh,int *x){
+    Element* novi=*vrh;
+    *x=(*vrh)->podatak;
+    (*vrh)=(*vrh)->sledeci;
+    free(novi);
 }
 
-int main(){
-	Element *stack=NULL;
-	int i, x;
-	for(i=0;i<5;i++){
-		printf("Dodaj X na stack: ");
-		scanf("%i", &x);
-		push(&stack, x);
-	}
-	printf("_____________________\n");
-	stampaj(stack);
-	printf("\n_____________________\n");
-	pop(&stack, &x);
-	printf("%i", x);
-	return 0;
+
+Element *popPovratni(Element *vrh, int *x){
+    Element* novi=vrh;
+    *x=vrh->podatak;
+    vrh=vrh->sledeci;
+    free(novi);
+    return vrh;
+}
+
+int max(Element *vrh){
+    int max=vrh->podatak;
+    while(vrh!=NULL)
+    {
+        if(vrh->podatak > max){
+            max=vrh->podatak;
+        }
+    vrh=vrh->sledeci;
+    }
+    return max;
+}
+
+int main()
+{
+    Element *vrh=NULL;
+    int n, x;
+    printf("Unesite koliko elemenata zelite: ");
+    scanf("%i",&n);
+    for(int i=0;i<n;i++){
+        printf("Unesite podatak za cvor: ");
+        scanf("%i", &x);
+        vrh=push(vrh, x);
+    }
+
+    int max1=max(vrh);
+    int a;
+    printf("\nMaximum na stackuje %d\n", max1);
+    while(vrh->podatak!=max1){
+        vrh=popPovratni(vrh, &a);
+        printf("%i\t",a);
+    }
+    return 0;
 }
