a4a353734406c464a0812978803a21f91b8a97c6
kripticni <157402936+kripticni@users.noreply.github.com>
2024-05-24 12:15:06 +0200

    Gradivo sa casa, moje implementacije.

--- Binarno-Stablo/izvorniKod.c
+++ Binarno-Stablo/izvorniKod.c
@@ -75,9 +75,51 @@ int proizvod_manjih(Cvor* stablo){
   return proizvod_manjih(stablo->levi)*proizvod_manjih(stablo->desni);
 }
 
+int brojE(Cvor *koren, int e){
+    if(koren==NULL)return 0;
+    if(koren->podatak>e) return 1+brojE(koren->levi,e)+brojE(koren->desni,e);
+    return brojE(koren->levi,e)+brojE(koren->desni,e);
+}
+
+void listovi(Cvor* stablo){
+  if(stablo->levi!=NULL){
+    listovi(stablo->levi);
+    if(stablo->desni!=NULL){
+      listovi(stablo->desni);
+    }
+    return;
+  }
+  if(stablo->desni!=NULL){
+      listovi(stablo->desni);
+      return;
+    }
+    printf("%i\t", stablo->podatak);
+}
+
+Cvor* nadjiR(Cvor* stablo, int x){
+    if(stablo==NULL||stablo->podatak==x)return stablo;
+    if(stablo->podatak>x)return nadjiR(stablo->levi, x);
+    return nadjiR(stablo->desni, x);
+}
+
+Cvor* nadji(Cvor* koren, int x){
+    while(koren!=NULL && koren->podatak!=x){
+        if(x<koren->podatak)koren=koren->levi;
+            else koren=koren->desni;
+    }
+    return koren;
+}
+
+Cvor* maksCvorR(Cvor* stablo){
+    if(stablo->desni==NULL)return stablo;
+    return maksCvorR(stablo->desni);
+}
+
 int main(){
   Cvor* stablo=NULL;
-  int i, x, n;
+  int i, x, n, temp, e;
+  printf("Unesi temp");
+  scanf("%i",&temp);
   printf("Unesi broj cvorova: ");
   scanf("%i", &n);
   for(i=0;i<n;i++){
@@ -88,6 +130,14 @@ int main(){
   stampajInOrder(stablo);
   printf("\n");
   stampajAps(stablo);
-  printf("\nVelicina stabla je %i, proizvod cvorova je %i, suma cvorova je %i, proizvod manjih od 10 je %i\n", velicina(stablo), proizvod(stablo), suma(stablo), proizvod_manjih(stablo));
+  printf("\nUnesite E: ");
+  scanf("%i", &e);
+  printf("\nVelicina stabla je %i, proizvod cvorova je %i, suma cvorova je %i, proizvod manjih od 10 je %i\nBroj vecih od E je %i", velicina(stablo), proizvod(stablo), suma(stablo), proizvod_manjih(stablo), brojE(stablo, e));
+  //listovi(stablo);
+  Cvor* stablo2=nadji(stablo,e);
+  if(stablo2!=NULL){
+        printf("\nPostoji E.");
+  }else printf("\nNepostoji E.");
+
   return 0;
 }
