02f6ac1e0866247e8b7699ce6d4fb5c27b06ad43
kripticni <157402936+kripticni@users.noreply.github.com>
2024-06-07 12:06:12 +0200

    Dodato gradivo iz druge godine sa podrskom za liste.

--- Datoteke.c
+++ Datoteke.c
@@ -1,4 +1,4 @@
-
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -13,7 +13,8 @@ void citaj(Teniser *pok){
     scanf("%d", &pok->sifra);
     while (getchar()!='\n');
     if (pok->sifra!=0)
-    {gets(pok->ime);
+    {
+        gets(pok->ime);
         scanf("%d", &pok->broj);
     }
 }
@@ -27,21 +28,58 @@ void sortiraj (Teniser x[], int n){
     int i, j;
     Teniser pom;
     for (i=0;i<n-1;i++)
-    for (j=0;j<n-i-1;j++)
-    if (x[j-1].broj < x[j].broj){
-        pom=x[j-1];
-        x[j-1]=x[j];
-        x[j]=pom;
+    for (j=1;j<n-i-1;j++)
+        if (x[j-1].broj < x[j].broj){
+            pom=x[j-1];
+            x[j-1]=x[j];
+            x[j]=pom;
+        }
+}
+
+typedef struct element{
+    int sifra;
+    char ime[32];
+    int broj;
+    struct element *sledeci;
+}Element;
+
+Element *dodaj(Element *lista, int sifra, char ime[], int broj){
+    Element *novi=(Element*)malloc(sizeof(Element));
+    if (novi==NULL)
+    {
+        printf("Greska");
+        exit(0);
+    }
+    novi->sifra=sifra;
+    novi->broj=broj;
+    strcpy(novi->ime,ime);
+    novi->sledeci=NULL;
+    Element *pom=lista;
+    if (lista==NULL) return novi;
+    while (pom->sledeci!=NULL) pom=pom->sledeci;
+    pom->sledeci=novi;
+    return lista;
+}
+
+void stampaj(Element *lista)
+{
+    while (lista!=NULL)
+    {
+        printf("\t%d\t%s\t%d\n", lista->sifra, lista->ime, lista->broj);
+        lista=lista->sledeci;
     }
 }
 
-int main(){
+
+int main()
+{
     FILE *dat;
     Teniser t, a[30];
     dat=fopen("Teniseri.txt","w+");
     int i=0, n;
     citaj(&t);
-    while (t.sifra!=0){
+    while (t.sifra!=0)
+    {
         fprintf(dat, "%d\t%s\t%d\n", t.sifra, t.ime, t.broj);
         i++;
         //printf("\n Unesite za sledeceg\n");
@@ -50,10 +88,11 @@ int main(){
     n=i;
     i=0;
     fseek(dat, 0, SEEK_SET);
-    fscanf(dat, "%d\t%s\t%d", &a[i].sifra, a[i].ime, &a[i].broj);
-    while (!feof(dat)){
+    fscanf(dat, "%d\t%s\t%d\n", &a[i].sifra, a[i].ime, &a[i].broj);
+    while (!feof(dat))
+    {
         i++;
-        fscanf(dat, "%d\t%s\t%d", &a[i].sifra, a[i].ime, &a[i].broj);
+        fscanf(dat, "%d\t%s\t%d\n", &a[i].sifra, a[i].ime, &a[i].broj);
     }
     sortiraj(a,n);
     printf("\nSpisak Tenisera iz datoteke Teniseri.txt koji je uredjen u rastuci red je: \n");
@@ -62,5 +101,10 @@ int main(){
     printf("\nMin poena = %d\n", a[n-1].broj);
     printf("\nRazlika max i min broja poena je = %d\n", a[0].broj-a[n-1].broj);
     fclose(dat);
+
+    Element *lista=NULL;
+    for (i=0;i<n;i++) lista=dodaj(lista, a[i].sifra, a[i].ime, a[i].broj);
+    printf("\nElementi liste su :\n");
+    stampaj(lista);
     return 0;
 }
