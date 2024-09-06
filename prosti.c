fe03c396ba5fb66b693eb0d4b8c315def3119504
kripticni <kripticno@gmail.com>
2024-09-06 16:37:47 +0200

    Dodato eratostenovo sito, implementacija sa for i optimizacije.

--- prosti.c
+++ prosti.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <Stdio.h>
-#include <math.h>
+#include <stdlib.h>
+#include <stdbool.h> //za bitset
 
 int prost1(int n){
     int i=2;
@@ -22,16 +22,58 @@ int prost2(int n){
 
 int prost3(int n){
     int i=2;
-    while(i<=sqrt(n)){
+    while(i*i<=n){
         if(n%i==0) return 0;
         i++;
     }
     return 1;
 }
 
+int prost1for(int n){
+    int i=2;
+    for(;i<=n-1;i++){
+        if(n%i==0) return 0;
+    }
+    return 1;
+}
+
+int prost2for(int n){
+    int i=2;
+    for(;i<=n/2;i++){
+        if(n%i==0) return 0;
+    }
+    return 1;
+}
+
+int prost3for(int n){
+    int i=2;
+    for(;i*i<=n;i++){
+        if(n%i==0) return 0;
+    }
+    return 1;
+}
+
+void EratostenovoSito(int n, int* stanja){
+  int i, p=2;
+  for(;p*p<=n;p++){
+    if(stanja[p] == false) {
+      for (i =p*p; i<=n; i+=p)
+        stanja[i] = true;
+    }
+  }
+  for (p = 2; p <= n; p++)
+    if(!stanja[p])
+      printf("%d ",p);
+}
+
 int main(){
-    (prost1(13))?printf("Prost.\n"):printf("Nije prost.\n");
-    (prost2(13))?printf("Prost.\n"):printf("Nije prost.\n");
-    (prost3(13))?printf("Prost.\n"):printf("Nije prost.\n");
-    return 0;
+  int n;
+  printf("Unesite broj: ");
+  scanf("%i", &n);
+  (prost1(n) && prost2(n) && prost3(n) && prost1for(n) && prost2for(n) && prost3for(n))?printf("Prost broj.\n"):printf("Nije prost broj.\n");
+  printf("Unesi broj do koga se traze prosti: ");
+  scanf("%i", &n);
+  int* prosti = (int*)calloc(n, sizeof(int));
+  EratostenovoSito(n, prosti);
+  return 0;
 }
